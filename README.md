0429b551a9110478aedec019082c2c80a5766a71
andre.jorgensen <andre.jorgensen@tidal.com>
2019-09-18 10:19:28 +0200

    Tweak UnitTest

--- README.md
+++ README.md
@@ -7,13 +7,12 @@
 [![codebeat badge](https://codebeat.co/badges/deb313b8-68c2-4e79-8bed-dfcdfb79416f)](https://codebeat.co/projects/github-com-eonist-paginationservice-master)
 
 # PaginationService
-
 <img width="240" alt="img" src="pagination.gif?raw=true">
 
 ### What is it
 - A framework for mocking paginated calls.
 
-### requirements:
+### Requirements:
 - Decodable struct
 - .json file
 - A subclass with Type and path to json file
--- src/PaginationService.swift
+++ src/PaginationService.swift
@@ -2,14 +2,18 @@ import Foundation
 import FileSugar
 import JSONSugar
 /**
- * - Parameter _items: caches the parsed items, We have to have this variable here, or else subclass wont have anything to override, also since >greneric classes cant have static stroage variables, only static get / set variables
+ * - Parameter _items:
  * - Parameter networkLatency: general network response time
  * - Parameter filePath: the resources to load from
  * - Parameter items: loads from file if items is not stored in cache yet
  * - Fixme: ⚠️️ rename items to getItems() and _items to items, and __items to _items ?
  */
 open class PaginationService<T: Decodable> {
-   open class var _items: [T]? { get { fatalError("Must be overriden in subclass") } set { _ = newValue; fatalError("Must be overriden in subclass") } }
+   /**
+    * Caches the parsed items, We have to have this variable here, or else subclass wont have anything to override, also since greneric classes can't have static stroage variables, only static get / set variables
+    * - Abstract: The idea is that this variable is overriden in the subclass with the Speccified Generic type
+    */
+   open class var _items: [T]? { get { fatalError("Must be overriden in subclass") } set { _ = newValue; fatalError("Must be overriden in subclass") } } // We have to asign the newValue to void, or else linting complains
    open class var networkLatency: Range<Int> { return (1..<4) }
    open class var filePath: String { fatalError("Must be overriden in subclass") }
    open class var items: [T] {
